e380b57cd9dff9f3cd1fef480fae13a10c75feec
LaurynasMot <laurynas.mot@gmail.com>
2020-03-05 18:32:48 +0200

    initial commit

---
+++ src/test/java/com/banking/bankstatements/BankStatementsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking.bankstatements;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankStatementsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
